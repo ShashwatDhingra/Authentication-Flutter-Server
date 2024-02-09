57437d39942f72ea95a70b71935cd876f105058a
Shashwat <shashwatdhingra2@gmail.com>
2024-02-09 22:19:51 +0530

    add getUser route

--- routes/auth.js
+++ routes/auth.js
@@ -54,11 +54,35 @@ Router.post('/signin', async (req, res) => {
 
         const token = jwt.sign({ id: existingUser._id }, 'jwtKey');
 
-        res.json({token, ...existingUser._doc});
+        res.json({ token, ...existingUser._doc });
 
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
 })
+
+// verify token
+Router.post('/tokenIsValid', async (req, res) => {
+   try{
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).json(false);
+
+    const isVerified = jwt.verify(token, 'jwtKey');
+    if(!isVerified) return res.status(401).json(false);
+
+    const user = await User.findById(isVerified.id);
+    if(!user) return res.status(401).json(false);
+
+    return res.json(true);
+   }catch(e){
+    res.status(500).json({error: e.message});
+   } 
+});
+
+// get user data
+Router.get('/', auth, async (req, res) => {
+    const user = await User.findOne(req.user);
+    res.json({ ...user._doc, token: req.ltoken });
+});
 
 module.exports = Router;
